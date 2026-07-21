fbba635b9f693be95b3aa13152f56c93b380f2c9
unknown <tappetlavedavathi@gmail.com>
2026-07-21 21:40:28 +0530

    Added hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("hello world")
+def greet(name):
+ return f"Hello, {name}! Welcome to Git and GitHub."
+if __name__ == "__main__":
+ user_name = input("Enter your name: ")
+ print(greet(user_name))
